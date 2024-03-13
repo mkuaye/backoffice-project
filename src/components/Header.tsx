c0b69a17e8831a9749f816e5919eb3de8c02cc29
Matheus Kuaye Muramoto <matheusmuramoto@gmail.com>
2024-03-13 20:22:37 -0300

    :recycle: fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import { IoMenu } from 'react-icons/io5';
+
 export function Header() {
+  const [openLeftMenu, setOpenLeftMenu] = useState<boolean>(false);
+
+  console.log('openLeftMenu', openLeftMenu);
+  const toggleLeftMenu = (): void => {
+    setOpenLeftMenu(!openLeftMenu);
+  };
+
   return (
-    <h1 className='text-3xl font-bold underline bg-red-600'>Hello world!</h1>
+    <div className='flex h-fit justify-center items-center '>
+      <div className='flex justify-between w-11/12 items-center'>
+        <button onClick={toggleLeftMenu}>
+          <IoMenu className='size-12' />
+        </button>
+        <h1 className='text-4xl text-center py-5 font-semibold'>
+          Backoffice Example
+        </h1>
+        <div />
+      </div>
+    </div>
   );
 }
